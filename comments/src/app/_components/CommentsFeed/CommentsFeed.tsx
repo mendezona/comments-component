9135b7de6d38e078b3e600c58508136de0556371
Joseph Mendezona <joseph_mendezona@hotmail.com>
2024-07-01 18:02:36 +0200

    make layout responsive

--- comments/src/app/_components/CommentsFeed/CommentsFeed.tsx
+++ comments/src/app/_components/CommentsFeed/CommentsFeed.tsx
@@ -13,10 +13,11 @@ export default function CommentsFeed(): JSX.Element {
     queryKey: [LOCAL_STORAGE_ALL_COMMENTS_KEY],
     queryFn: async () => getCommentsFromLocalStorage(),
   });
+  const skeletons: unknown[] = Array.from({ length: 3 });
 
   return (
-    <div className="container mx-auto w-full">
-      <div className="mx-auto w-full flex-col p-10 md:w-3/5 md:px-5 md:py-10">
+    <div className="mx-auto flex w-full">
+      <div className="mx-auto w-full flex-col px-3 py-5 md:w-3/5 md:p-10 md:px-5 md:py-10">
         <h2 className="mb-10 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0">
           Comments Demo App
         </h2>
@@ -26,17 +27,20 @@ export default function CommentsFeed(): JSX.Element {
         <div className="w-full border-b-2 pb-5">
           <CommentForm />
         </div>
-        <div className="w-full">
+        <div className="mt-3 w-full">
           {isLoading ? (
-            <div className="flex items-center space-x-4">
-              {/* TODO: Add skeletons back */}
-              <Skeleton className="h-12 w-12 rounded-full" />
-              <div className="space-y-2">
-                <Skeleton className="h-4 w-[150px]" />
-                <Skeleton className="h-4 w-[300px]" />
-                <Skeleton className="h-4 w-[270px]" />
-              </div>
-            </div>
+            <>
+              {skeletons.map((_, index) => (
+                <div key={index} className="mb-10 flex items-center space-x-4">
+                  <Skeleton className="h-12 w-12 rounded-full" />
+                  <div className="space-y-2">
+                    <Skeleton className="h-4 w-[150px]" />
+                    <Skeleton className="h-4 w-[200px] md:w-[400px]" />
+                    <Skeleton className="h-4 w-[180px] md:w-[350px]" />
+                  </div>
+                </div>
+              ))}
+            </>
           ) : !data ? (
             <h4 className="mb-3 scroll-m-20 text-xl font-semibold tracking-tight">
               No comments posted yet... be the first?
